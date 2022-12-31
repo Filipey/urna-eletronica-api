ac934dd3b07917534c6b6c254e2adb754b311255
Filipe <filipeasm18@gmail.com>
2022-12-31 14:21:39 -0300

    Refactor: Refactored picture seeding

--- prisma/seed/Candidates.ts
+++ prisma/seed/Candidates.ts
@@ -5,60 +5,71 @@ export const MockedCandidates: Candidate[] = [
     name: "Neymar da Silva Santos Júnior",
     number: 10,
     partyNumber: 26,
-    picture: "neymar.jpeg",
+    picture: "https://pbs.twimg.com/media/FiRRfNfXEAMCwmk.jpg",
     uf: "SP",
     role: "PRESIDENT",
     substituteName: "Vinícius José Paixão de Oliveira Júnior",
-    substitutePicture: "vinicius_junior.jpeg",
+    substitutePicture:
+      "https://imagens.ebc.com.br/TpI4S1_y1loZcneNfTMTVqRa8Sc=/1170x700/smart/https://agenciabrasil.ebc.com.br/sites/default/files/thumbnails/image/vinicius_junior_selecao_0.jpg?itok=qKHYjd2c",
   },
   {
     name: "Bruna Reis Maia",
     number: 6,
     partyNumber: 66,
-    picture: "bruna_marquezine.jpeg",
+    picture:
+      "https://nsc-total-wp.s3.sa-east-1.amazonaws.com/wp-content/uploads/2022/10/bruna-marquezine-supergirl-1.jpg",
     uf: "RJ",
     role: "PRESIDENT",
     substituteName: "Rafaella Freitas Ferreira de Castro Matthaus",
-    substitutePicture: "rafa_kalliman.jpeg",
+    substitutePicture:
+      "https://www.tvpop.com.br/wp-content/uploads/2022/12/rafa-kalimann-globo-foto-reproducao-tvpop.jpeg",
   },
   {
     name: "Arthur Lopes",
     number: 111,
     partyNumber: 4,
-    picture: "arthur_lopes.jpeg",
+    picture:
+      "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     uf: "MG",
     role: "GORVENOR",
     substituteName: "João do Arroz",
-    substitutePicture: "joao_arroz.jpeg",
+    substitutePicture:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
   },
   {
     name: "Ana Sophia Rocha",
     number: 90,
     partyNumber: 66,
-    picture: "ana_sophia.jpeg",
+    picture:
+      "https://images.unsplash.com/photo-1567532939604-b6b5b0db2604?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     uf: "MG",
     role: "GORVENOR",
     substituteName: "Carla do Tomate",
-    substitutePicture: "carla_tomate.jpeg",
+    substitutePicture:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
   },
   {
     name: "Rafael Costela",
     number: 44,
     partyNumber: 10,
-    picture: "rafael_costela.jpeg",
+    picture:
+      "https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     uf: "MG",
     role: "SENATOR",
     substituteName: "Gabriel Japones",
-    substitutePicture: "gabriel_japones.jpeg",
+    substitutePicture:
+      "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80",
   },
   {
     name: "Lívia Almeida",
     number: 76,
     partyNumber: 66,
-    picture: "livia_almeida.jpeg",
+    picture:
+      "https://images.unsplash.com/photo-1554727242-741c14fa561c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     uf: "MG",
     role: "SENATOR",
     substituteName: "Letícia Fazendeira",
-    substitutePicture: "leticia_fazenda.jpeg",
+    substitutePicture:
+      "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
   },
 ];
--- prisma/seed/Persons.ts
+++ prisma/seed/Persons.ts
@@ -5,119 +5,132 @@ export const MockedPersons: Person[] = [
     name: "Filipe Augusto Santos de Moura",
     cpf: "12345678910",
     uf: "MG",
-    picture: "filipe.jpeg",
-    hasVoted: false
+    picture: "https://avatars.githubusercontent.com/u/85424389?v=4",
+    hasVoted: false,
   },
   {
     name: "Diogo Leite Lucas",
     cpf: "10987654321",
     uf: "MG",
-    picture: "diogo.jpeg",
-    hasVoted: false
+    picture: "https://avatars.githubusercontent.com/u/62341955?v=4",
+    hasVoted: false,
   },
   {
     name: "Neymar da Silva Santos Júnior",
     cpf: "1010101010101",
     uf: "SP",
-    picture: "neymar.jpeg",
-    hasVoted: false
+    picture: "https://pbs.twimg.com/media/FiRRfNfXEAMCwmk.jpg",
+    hasVoted: false,
   },
   {
     name: "Fausto Corrêa da Silva",
     cpf: "87878787878",
     uf: "SP",
-    picture: "faustao.jpeg",
-    hasVoted: false
+    picture:
+      "https://p2.trrsf.com/image/fget/cf/1200/1600/middle/images.terra.com/2021/06/17/1609069130-programa-do-faustao-globo-fautos-silva.jpeg",
+    hasVoted: false,
   },
   {
     name: "Rodrigo Alcatraz Faro",
     cpf: "54545454545",
     uf: "SP",
-    picture: "rodrigo-faro.jpeg",
-    hasVoted: false
+    picture:
+      "https://conteudo.imguol.com.br/c/parceiros/78/2022/12/17/rodrigo-faro-no-vai-dar-namoro-1671308735005_v2_1x1.jpg",
+    hasVoted: false,
   },
   {
     name: "Adenor Leonardo Bacchi",
     cpf: "29292929292",
     uf: "RS",
-    picture: "tite.jpeg",
-    hasVoted: false
+    picture:
+      "https://www.lance.com.br/files/article_main/uploads/2022/12/09/6393adf631239.jpeg",
+    hasVoted: false,
   },
   {
     name: "Celso Yunes Portiolli",
     cpf: "66666666666",
     uf: "PR",
-    picture: "celso-portiolli.jpeg",
-    hasVoted: false
+    picture:
+      "https://jpimg.com.br/uploads/2021/10/design-sem-nome-2021-10-07t110420.956.jpg",
+    hasVoted: false,
   },
   {
     name: "Alan Ferreira Pereira",
     cpf: "99999999599",
     uf: "SP",
-    picture: "alanzoka.jpeg",
-    hasVoted: false
+    picture:
+      "https://pbs.twimg.com/profile_images/944712346500587522/duOz-D8s_400x400.jpg",
+    hasVoted: false,
   },
   {
     name: "Maísa da Silva Andrade",
     cpf: "23234231281",
     uf: "SP",
-    picture: "maisa.jpeg",
-    hasVoted: false
+    picture:
+      "https://www.ofuxico.com.br/wp-content/uploads/2022/11/maisa-silva.jpg",
+    hasVoted: false,
   },
   {
     name: "Bruna Reis Maia",
     cpf: "89234174211",
     uf: "RJ",
-    picture: "bruna_marquezine.jpeg",
-    hasVoted: false
+    picture:
+      "https://nsc-total-wp.s3.sa-east-1.amazonaws.com/wp-content/uploads/2022/10/bruna-marquezine-supergirl-1.jpg",
+    hasVoted: false,
   },
   {
     name: "Manoela Latini Gavassi Francisco",
     cpf: "61236123909",
     uf: "SP",
-    picture: "manu_gavassi.jpeg",
-    hasVoted: false
+    picture: "https://www.rbsdirect.com.br/imagesrc/25748871.jpg?w=700",
+    hasVoted: false,
   },
   {
     name: "Vinicius Fernandes",
     cpf: "93437283123",
     uf: "PR",
-    picture: "vinicius_fernandes.jpeg",
-    hasVoted: false
+    picture:
+      "https://images.unsplash.com/photo-1615813967515-e1838c1c5116?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    hasVoted: false,
   },
   {
     name: "Lívia Almeida",
     cpf: "10111213141",
     uf: "MG",
-    picture: "livia_almeida.jpeg",
-    hasVoted: false
+    picture:
+      "https://images.unsplash.com/photo-1554727242-741c14fa561c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    hasVoted: false,
   },
   {
     name: "Arthur Lopes",
     cpf: "77777777771",
     uf: "MG",
-    picture: "arthur_lopes.jpeg",
-    hasVoted: false
+    picture:
+      "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    hasVoted: false,
   },
   {
     name: "Ana Sophia Rocha",
     cpf: "88888888881",
     uf: "MG",
-    picture: "ana_sophia.jpeg",
-    hasVoted: false
+    picture:
+      "https://images.unsplash.com/photo-1567532939604-b6b5b0db2604?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    hasVoted: false,
   },
   {
     name: "Rafael Costela",
     cpf: "99999999992",
     uf: "MG",
-    picture: "rafael_costela.jpeg",
-    hasVoted: false
+    picture:
+      "https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    hasVoted: false,
   },
   {
     name: "Yasmin da Conceição",
     cpf: "33333333335",
     uf: "MG",
-    picture: "yasmin_conceicao.jpeg",
-    hasVoted: false
+    picture:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    hasVoted: false,
   },
 ];
