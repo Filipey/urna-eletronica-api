3f2bc7aefaa8a870ea9b947603723406fd751b72
Filipe <filipeasm18@gmail.com>
2022-12-31 11:05:25 -0300

    Chore: Removed swagger and add Cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import { serve, setup } from "swagger-ui-express";
 import { appRouter } from "./routes";
 
 dotenv.config();
@@ -8,16 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-
-app.use(
-  "/docs",
-  serve,
-  setup(undefined, {
-    swaggerOptions: {
-      url: "/swagger.json",
-    },
-  })
-);
+app.use(cors());
 
 app.get("/api", (_req, res) => res.send("Hello from API!"));
 
